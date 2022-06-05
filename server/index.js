a0e1f23d35b9ae657622ac5dfc889483285cbbc6
kanokpc <kanokpc@gmail.com>
2022-06-05 14:10:47 +0700

    password protect

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const  CONNECTION_URL = 'mongodb://kanokpc:dod170678@cluster0-shard-00-00.w3avv.mongodb.net:27017,cluster0-shard-00-01.w3avv.mongodb.net:27017,cluster0-shard-00-02.w3avv.mongodb.net:27017/?ssl=true&replicaSet=atlas-f9u092-shard-0&authSource=admin&retryWrites=true&w=majority';
+const  CONNECTION_URL = 'mongodb://kanokpc:<password>@cluster0-shard-00-00.w3avv.mongodb.net:27017,cluster0-shard-00-01.w3avv.mongodb.net:27017,cluster0-shard-00-02.w3avv.mongodb.net:27017/?ssl=true&replicaSet=atlas-f9u092-shard-0&authSource=admin&retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
