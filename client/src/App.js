82cdca92b367e4fa87686874541d42b8f28837b7
kanokpc <kanokpc@gmail.com>
2022-06-05 13:56:01 +0700

    styles components

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 import csvicon from './images/csv-icon.png';
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography varian="h2" align="center">Memories</Typography>
-                <img src={csvicon} alt="csv" height="60" width="60"></img>
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} varian="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={csvicon} alt="csv" height="60" width="60"></img>
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     );
 }
