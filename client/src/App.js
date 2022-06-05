e859c5bbca637ad19dd4cf579a6d87800fed37ac
kanokpc <kanokpc@gmail.com>
2022-06-05 13:08:38 +0700

    add route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import csvicon from './images/csv-icon.png';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography varian="h2" align="center">Memories</Typography>
+                <img src={csvicon} alt="csv" height="60" width="60"></img>
+            </AppBar>
+        </Container>
     );
 }
 
--- server/index.js
+++ server/index.js
@@ -3,13 +3,17 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const  CONNECTION_URL = 'mongodb+srv://kanokpc:dod170678@cluster0.w3avv.mongodb.net/?retryWrites=true&w=majority';
+const  CONNECTION_URL = 'mongodb://kanokpc:dod170678@cluster0-shard-00-00.w3avv.mongodb.net:27017,cluster0-shard-00-01.w3avv.mongodb.net:27017,cluster0-shard-00-02.w3avv.mongodb.net:27017/?ssl=true&replicaSet=atlas-f9u092-shard-0&authSource=admin&retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
